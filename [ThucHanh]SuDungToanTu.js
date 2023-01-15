6d49af4dbec2f5cf41da7ee056139693f7ce2082
tn461151@gmail.com <113189474+jcthanhnguyen@users.noreply.github.com>
2023-01-15 21:48:59 +0700

    add su dung toan tu.js

---
+++ [ThucHanh]SuDungToanTu.js
@@ -0,0 +1,6 @@
+let inputW = prompt('Hãy nhập vào chiều rộng: ');
+let inputH = prompt('Hãy nhập vào chiều cao: ');
+let width = parseInt(inputW);
+let height = parseInt(inputH);
+let area = width * height;
+document.write('Diện tích hình chữ nhật là: ' + area);
